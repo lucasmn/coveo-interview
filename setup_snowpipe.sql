045d84462bb6653e6b3de6b783ca12d034c528bd
lucasmn <lucasmn@users.noreply.github.com>
2020-02-22 14:47:36 -0500

    Add files via upload

---
+++ setup_snowpipe.sql
@@ -0,0 +1,17 @@
+/* THIS SCRIPT SETS UP SNOWPIPE TO LISTEN FOR NOTIFICATIONS FROM AN S3 BUCKET AND INGEST RECORDS WHENEVER NEW IBJECTS ARE DROPPED INTO THE BUCKET */
+
+create or replace stage cov_interview.public.searches_staging
+url='s3://int-coveo' -- REPLACE THIS WITH A DIFFERENT BUCKET AS NEEDED
+credentials = (AWS_KEY_ID = 'AKIAIZBULSATZJDXGSLQ' AWS_SECRET_KEY = 'GjDsb/62aRIuUwApMsApTzHj9euwSFg90OpPLLR/'); -- REPLACE WITH ANOTHER AWS KEY AS NEEDED
+
+create or replace table cov_interview.public.searches(
+jsontext variant
+);
+
+create or replace pipe cov_interview.public.snowpipe auto_ingest=true as -- AUTO_INGEST=TRUE ACTIVATES NOTFICATION LISTENER 
+    copy into cov_interview.public.searches 
+    from @cov_interview.public.searches_staging 
+    file_format = (type = 'JSON');
+
+show stages;
+show pipes;
